ffd28db6d78d53415bbd89ec61c4a576377d369b
marystefi92 <maria.stefaniuk.ms@gmail.com>
2023-03-22 17:07:20 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,20 @@ function displayTemperature(response) {
   iconElem.setAttribute("alt", response.data.weather[0].main);
 }
 
-let apiKey = "b2d9fa1f2b35557e4615dd5fab218834";
-let city = "Stockholm";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function searchCity(city) {
+  let apiKey = "b2d9fa1f2b35557e4615dd5fab218834";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function submitCity(event) {
+  event.preventDefault();
+  let cityInputElem = document.querySelector("#city-input").value;
+  searchCity(cityInputElem);
+}
+
+searchCity("Stockholm");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submitCity);
